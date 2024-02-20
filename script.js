72744098deeace22e4b2ccb8d8cffa9ac8b89734
Prashant Sutar <prashant.work100@gmail.com>
2024-02-20 22:02:57 +0530

    some changes

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-document.addEventListener("click", function(event) {
+const headings = document.querySelectorAll('.heading');
 
-    document.querySelectorAll(".desc").forEach(a => {a.style.display = "none"});
+headings.forEach(heading => {
+  heading.addEventListener('click', () => {
+    const currentDesc = heading.parentNode.querySelector('.desc');
+    const allDescs = document.querySelectorAll('.desc');
 
-    const section= event.target.parentNode;
-    const desc = section.querySelector(".desc");
-    desc.style.display = "block";
-  
+    allDescs.forEach(desc => {
+      if (desc === currentDesc) {
+        desc.classList.toggle('fulldesc');
+      } else {
+        desc.classList.remove('fulldesc');
+      }
+    });
+  });
 });
